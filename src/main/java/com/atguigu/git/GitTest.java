af2dc14d5f455bf5e82c7c54763cf909a4436df7
zhai <zhai@hang.com>
2024-03-26 21:47:08 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("hello git2!");
         System.out.println("hello git3!");
         System.out.println("master test!");
+        System.out.println("push test!");
 
         Hello();
     }
