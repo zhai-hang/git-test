88aedd194e318305615aa6ff813c4d11f17480af
zhai <zhai@hang.com>
2024-03-26 21:24:36 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -1,6 +1,11 @@
 package com.atguigu.git;
 
 public class GitTest {
+
+    public void Hello(){
+        System.out.println("hello");
+    }
+
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git2!");
